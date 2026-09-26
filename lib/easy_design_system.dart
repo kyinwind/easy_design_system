5a216613083c44d4d437f523085fa2d3a3e19177
自在清风 <xuehui.yang888@icloud.com>
2026-09-27 00:07:39 +0800

    feat: export EDS dialogs

--- lib/easy_design_system.dart
+++ lib/easy_design_system.dart
@@ -27,6 +27,7 @@ export 'src/widgets/eds_button.dart';
 export 'src/widgets/eds_card.dart';
 export 'src/widgets/eds_collapsible_section.dart';
 export 'src/widgets/eds_comparison_section.dart';
+export 'src/widgets/eds_dialogs.dart';
 export 'src/widgets/eds_group.dart';
 export 'src/widgets/eds_form_controls.dart';
 export 'src/widgets/eds_hero_panel.dart';
